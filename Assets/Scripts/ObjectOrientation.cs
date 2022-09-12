cff3d53b66dfb2ea14b3dbce19fbd05e83edcc1f
Jorge Cobacho_PC <108235595+Jhcob@users.noreply.github.com>
2022-09-12 13:20:01 +0200

    Adding unity project

---
+++ Assets/Scripts/ObjectOrientation.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class ObjectOrientation : MonoBehaviour
+{
+    public struct Vector3
+    {
+        public float x;
+        public float y;
+        public float z;
+        
+    }
+}
